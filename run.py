6cc2d4c7a51ac7671157ea49f32f187f402d396c
jmmymnn-py <jmmymnn@proton.me>
2025-05-29 21:57:56 -0700

    shortened imports to not look like a fkin moron

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ import pytz
 
 ## Importing functions ---------------------------------------------------------------------##
 
-from scrape_Gilman import scrape_Gilman
-from scrape_Elis import scrape_Elis
-from scrape_Stork import scrape_Stork
-from combine import combine
-from enrich import enrich
-from cache import load_bandcamp_lookup, need_cache, create_cache, read_cache, when_updated
+import scrape_Gilman
+import scrape_Elis
+import scrape_Stork
+import combine
+import enrich
+import cache
 from run_Streamlit import run_Streamlit
 
 CACHE_FILE = "cached_df.csv"
